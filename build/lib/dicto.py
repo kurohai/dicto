701ac0b65d6f05e347c67b48207d5bd1f4a1e631
kurohai <kurohai@gmail.com>
2015-11-03 09:40:01 -0600

    backup of current working files

---
+++ build/lib/dicto.py
@@ -0,0 +1,15 @@
+#!/bin/env python
+
+
+class dicto(dict):
+
+    def __init__(self, *args, **kwargs):
+        super(dicto, self).__init__(*args, **kwargs)
+        self.__dict__ = self
+
+    def export(self):
+        """exports all attributes to a true dict"""
+        d = dict()
+        for k, v in self.items():
+            d[k] = v
+        return d
